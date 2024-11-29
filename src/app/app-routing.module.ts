362b764f40cf5755e63dd4b1bfb1148a26e67355
vinir <46539805+malkyn@users.noreply.github.com>
2024-11-29 11:26:28 -0300

    Correção dropdown e estrutura html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstPageComponent } from './first-page/first-page.component';
-import { RecommendationsComponent } from './recommendations/recommendations.component';
+import { RecommendationsComponent } from './recommendations/recommendations/recommendations.component';
 import { MultiselectComponent } from './multiselect/multiselect.component';
+import { LayoutComponent } from './layout/layout/layout.component';
 
 const routes: Routes = [
+  {path: '',component: FirstPageComponent,pathMatch:"full"},
   {
     path: '',
-    component: FirstPageComponent,
-    pathMatch:"full"
-  },
-  {
-    path: 'recommendations',
-    component: RecommendationsComponent
-  },
-  {
-    path: 'multiselect',
-    component: MultiselectComponent
+    component: LayoutComponent,
+    children: [
+      { path: 'recommendations', component: RecommendationsComponent },
+      { path: 'multiselect', component: MultiselectComponent }
+    ]
   }
 ];
 
